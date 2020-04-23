529add63ee3b62efb6c158df651f801cf20cfb48
root <root@bastion.wspark.com>
2020-04-23 13:45:10 +0900

    ok

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
 git commit -m 'ok'
-git push --repo https://wspark:qkrwls9@github.com/wspark/simple-war-eap64.git
+git push --repo https://wspark:qkrwlsA1@github.com/wspark/simple-war-eap64.git
